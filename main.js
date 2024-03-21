81ad8ea7699db07753d3e2f592074aab6421d2d9
Yasin Türk <yasinturk03@gmail.com>
2024-03-21 23:56:34 +0300

    feat: next button js

--- main.js
+++ main.js
@@ -1,24 +1,35 @@
 const productContainer = document.querySelector(".product-container");
 const productCard = document.querySelectorAll(".product-card");
-const buttonNext = document.querySelector("product-container-right-button");
+const buttonNext = document.querySelector(".product-container-right-button");
+
 let counter = 0;
-const slideWidth = productCard[0].clientWidth * 2;
+const slideWidth = productCard[0].clientWidth;
 
 function moveSlider(direction) {
-  if (counter === productCard.length) {
-   
-  }
-  if (direction === "left") {
+  if (direction === "next") {
+    counter++;
+    if (counter === productCard.length) {
+      counter = 0;
+    }
+  } else if (direction === "prev") {
     counter--;
     if (counter < 0) {
-      counter = productCard.length - 2;
-    }
-  } else if (direction === "right") {
-    counter++;
-    if (counter >= productCard.length - 1) {
-      counter = 0;
+      counter = productCard.length - 1;
     }
   }
   const offset = -slideWidth * counter;
   productContainer.style.transform = `translateX(${offset}px)`;
 }
+
+productCard.forEach((card) => {
+  const colorButtons = card.querySelectorAll(".product-color button");
+
+  colorButtons[0].classList.add("active");
+
+  colorButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+      colorButtons.forEach((btn) => btn.classList.remove("active"));
+      button.classList.add("active");
+    });
+  });
+});
